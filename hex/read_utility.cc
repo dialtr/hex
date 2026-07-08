0aff4f5b723e3de52602130af551930525fab9a5
Thomas R. Dial <dialtr@gmail.com>
2026-07-07 23:59:16 -0400

    add utility function to extract the start character from a stream

--- hex/read_utility.cc
+++ hex/read_utility.cc
@@ -1,6 +1,28 @@
 // Copyright (C) 2026 the hex authors. All rights reserved.
 #include "hex/read_utility.h"
 
+#include <cctype>
+#include <cstdint>
+#include <istream>
+#include <string>
+
+#include "absl/status/status.h"
+
 namespace hex {
-//
+
+absl::Status ReadStartByte(std::istream& input) {
+  for (;;) {
+    int c = input.get();
+    if (c == std::char_traits<char>::eof()) {
+      return absl::ResourceExhaustedError(
+          "ReadStartByte: reached the end of the input stream before "
+          "reading ':'");
+      if (c == ':') {
+        break;
+      }
+    }
+  }
+  return absl::OkStatus();
+}
+
 }  // namespace hex
--- hex/read_utility.h
+++ hex/read_utility.h
@@ -2,8 +2,20 @@
 #ifndef HEX_READ_UTILITY_H_
 #define HEX_READ_UTILITY_H_
 
+#include <cstdint>
+#include <istream>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+
 namespace hex {
-//
+
+// Read characters from the stream until the start character ':' is read.
+// Returns absl::OkStatus() on success.
+// Returns absl::ResourceExhaustedError() if EOF is reached before the
+//   the char is found.
+absl::Status ReadStartByte(std::istream& input);
+
 }  // namespace hex
 
 #endif  // HEX_READ_UTILITY_H_
