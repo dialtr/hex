00ab96aaade49411d81875bc7479f1e0a2bbed8b
Thomas R. Dial <dialtr@gmail.com>
2026-07-08 00:17:25 -0400

    add test

--- hex/read_utility_test.cc
+++ hex/read_utility_test.cc
@@ -9,8 +9,16 @@
 #include "absl/status/status.h"
 
 TEST(ReadUtilityTest, ReadStartByte_FindsStartByte) {
-  std::stringstream line("    :");
-  absl::Status status;
-  status = hex::ReadStartByte(line);
+  std::stringstream line("    :x");
+  const absl::Status status = hex::ReadStartByte(line);
   EXPECT_EQ(absl::OkStatus(), status);
+  const char next = line.get();
+  // Make sure the ':' was consumed, but no further.
+  EXPECT_EQ('x', next);
+}
+
+TEST(ReadUtilityTest, ReadStartByte_ReturnsResourceExhausted) {
+  std::stringstream line("    ");
+  const absl::Status status = hex::ReadStartByte(line);
+  EXPECT_EQ(absl::StatusCode::kResourceExhausted, status.code());
 }
